fc39e3cd6a7981bbb32d4a7dd1264582f13cb599
adelakim <diddnjs02@likelion.org>
2020-12-08 13:28:24 +0900

    Create printFirst() in test3.js
    - 큐브 상태 중 첫번째 줄 출력하는 함수 구현

--- test3.js
+++ test3.js
@@ -1,8 +1,66 @@
-function Person(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
+const rl = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const promptMsg = "CUBE> ";
+const order = {
+  front: "F",
+  right: "R",
+  up: "U",
+  back: "B",
+  left: "L",
+  down: "D",
+  counterFront: "F'",
+  counterRight: "R'",
+  counterUp: "U'",
+  counterBack: "B'",
+  counterLeft: "L'",
+  counterDonw: "D'",
+};
+const cube = {
+  f: [
+    ["O", "O", "O"],
+    ["O", "O", "O"],
+    ["O", "O", "O"],
+  ],
+  r: [
+    ["G", "G", "G"],
+    ["G", "G", "G"],
+    ["G", "G", "G"],
+  ],
+  u: [
+    ["B", "B", "B"],
+    ["B", "B", "B"],
+    ["B", "B", "B"],
+  ],
+  b: [
+    ["Y", "Y", "Y"],
+    ["Y", "Y", "Y"],
+    ["Y", "Y", "Y"],
+  ],
+  l: [
+    ["W", "W", "W"],
+    ["W", "W", "W"],
+    ["W", "W", "W"],
+  ],
+  d: [
+    ["R", "R", "R"],
+    ["R", "R", "R"],
+    ["R", "R", "R"],
+  ],
+};
+rl.setPrompt(printFirst(cube.f) + "\n" + promptMsg);
+rl.prompt();
+rl.on("line", (line) => {});
+rl.on("close", () => {
+  process.exit();
+});
+
+function printFirst(arr) {
+  let res = "";
+  const space = "                 ";
+  for (let i = 0; i < arr.length; i++) {
+    res += space + arr[i].join(" ") + "\n";
+  }
+  return res;
 }
-
-let me = new Person("step-3", "Commit");
-
-console.table(me);
