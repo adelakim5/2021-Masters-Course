ce15cae6162490d6dba90219f09c0d112ed3f2d1
adelakim <diddnjs02@likelion.org>
2020-12-08 15:29:18 +0900

    Create turnToRightDirection()
    - U, D' 명령어 입력시 수행하는 turnToRightDirection() 구현
    - U는 front, right, back, left면에서 1번째 행을, D'는 마지막 행을 같은
    방향으로 움직이기 때문에 함수 하나로 수행

--- test3.js
+++ test3.js
@@ -3,19 +3,21 @@ const rl = require("readline").createInterface({
   output: process.stdout,
 });
 const promptMsg = "CUBE> ";
-const order = {
+const rightOrder = {
   front: "F",
   right: "R",
   up: "U",
   back: "B",
   left: "L",
   down: "D",
-  counterFront: "f",
-  counterRight: "r'",
-  counterUp: "u'",
-  counterBack: "b'",
-  counterLeft: "l'",
-  counterDonw: "d'",
+};
+const countOrder = {
+  front: "f",
+  right: "r",
+  up: "u'",
+  back: "b",
+  left: "l",
+  down: "d",
 };
 const cube = {
   f: [
@@ -51,27 +53,52 @@ const cube = {
 };
 rl.setPrompt(printSide(cube.u, "") + printCenter(cube.l, cube.f, cube.r, cube.b, "") + printSide(cube.d, "") + promptMsg);
 rl.prompt();
-rl.on("line", (line) => {});
+rl.on("line", (line) => {
+  line = parse(line);
+  for (let i = 0; i < line.length; i++) {
+    const letter = line[i] === "2" ? line[i - 1] : line[i];
+    if (letter === rightOrder.up || letter === countOrder.down) {
+      console.log(letter);
+      [cube.f, cube.r, cube.b, cube.l] = turnToRightDirection(cube.f, cube.r, cube.b, cube.l, letter);
+    }
+    console.log(letter + "\n" + printSide(cube.u, "") + printCenter(cube.l, cube.f, cube.r, cube.b, "") + printSide(cube.d, ""));
+  }
+});
 rl.on("close", () => {
   process.exit();
 });
 
-function parse(letters) {
-  return letters.replace(/F'/g, "f").replace(/R'/g, "r").replace(/U'/g, "u").replace(/B'/g, "b").replace(/L'/g, "l").replace(/D'/g, "d");
+function turnToRightDirection(front, right, back, left, letter) {
+  const row = letter === rightOrder.up ? 0 : 2;
+  const total = [...front[row], ...right[row], ...back[row], ...left[row]];
+  const temp = total[0];
+  for (let i = 1; i < total.length; i++) {
+    total[i - 1] = total[i];
+  }
+  total[total.length - 1] = temp;
+  front[row] = total.slice(0, 3);
+  right[row] = total.slice(3, 6);
+  back[row] = total.slice(6, 9);
+  left[row] = total.slice(9, total.length);
+  return [front, right, back, left];
 }
 
-function printSide(arr, res) {
+function parse(line) {
+  return line.replace(/F'/g, "f").replace(/R'/g, "r").replace(/U'/g, "u").replace(/B'/g, "b").replace(/L'/g, "l").replace(/D'/g, "d");
+}
+
+function printSide(arr, str) {
   const space = "               ";
   for (let i = 0; i < 3; i++) {
-    res += space + arr[i].join(" ") + "\n";
+    str += space + arr[i].join(" ") + "\n";
   }
-  return res + "\n";
+  return str + "\n";
 }
 
-function printCenter(arr1, arr2, arr3, arr4, res) {
+function printCenter(arr1, arr2, arr3, arr4, str) {
   const space = "     ";
   for (let i = 0; i < 3; i++) {
-    res += arr1[i].join(" ") + space + arr2[i].join(" ") + space + arr3[i].join(" ") + space + arr4[i].join(" ") + "\n";
+    str += arr1[i].join(" ") + space + arr2[i].join(" ") + space + arr3[i].join(" ") + space + arr4[i].join(" ") + "\n";
   }
-  return res + "\n";
+  return str + "\n";
 }
