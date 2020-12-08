4e126883381209f33922e28da15e6540b7fae78e
adelakim <diddnjs02@likelion.org>
2020-12-08 10:06:15 +0900

    Create test1.js & readme.md
    - 사용자로부터 입력받은 단어, 정수, 이동 방향에 맞게 해당 단어를 해당
    정수만큼 이동시키는 프로그램 작성
    - 소문자로 들어오는 경우를 대비하기 위해 이동 방향은 무조건 대문자로
    변환시키도록 함
    - 정수가 음수인 경우 해당 절댓값만큼 이동 방향의 반대로 움직이기 때문에
    음수를 양수로, 방향을 반대로 변환시킴
    - 정수가 단어 길이의 배수가 아닐때만 실행하는 move()를 구현하여 코드
    독성을 높임
    - hoisting을 사용하기 위해 function으로 함수 구현

--- test1.js
+++ test1.js
@@ -1,1 +1,41 @@
-console.log("step-1 commit test");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const promptMsg = "> ";
+rl.setPrompt("단어 하나, 정수 숫자 하나(-100 <= N <100), L 또는 R을 입력하세요. \n ex) apple 3 R \n ※L, R은 소문자도 가능 \n" + promptMsg);
+rl.prompt();
+rl.on("line", (line) => {
+  let [word, num, direct] = line.split(" ");
+  num = +num;
+  direct = direct.toUpperCase();
+  if (num < 0) {
+    direct = direct === "L" ? "R" : "L";
+    num *= -1;
+  }
+  if (num % word.length) word = move(direct, num, word.split(""));
+  console.log(word);
+  rl.setPrompt(promptMsg);
+  rl.prompt();
+});
+rl.on("close", () => {
+  process.exit();
+});
+
+function move(direct, num, wordArr) {
+  for (let i = 0; i < num; i++) {
+    let temp = "";
+    switch (direct) {
+      case "L":
+        temp = wordArr.shift();
+        wordArr.push(temp);
+        break;
+      case "R":
+        temp = wordArr.pop();
+        wordArr.unshift(temp);
+        break;
+    }
+  }
+  return wordArr.join("");
+}
