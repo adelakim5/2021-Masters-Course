c98976a9539faea0e9ed65b55927bd82d71163b7
adelakim <diddnjs02@likelion.org>
2020-12-08 10:41:47 +0900

    Create test2.js & readme.md
    - 초기 프롬프트 메시지와 큐브 모양을 사용자에게 입력받기 전 출력해야
    하므로 전역변수로 둠
    - 명령어는 고정되어있고 공통적으로 사용하기 때문에 전역변수로 둠
    - 기능을 하는 함수들을 각각 만듦으로써 가독성을 높임
    	- doQuit() : Bye~를 출력하고 끝내는 함수
    	- parse() : 모든 명령어의 length를 1로 만드는 함수
    	- recover() : parse한 명령어를 원래 상태로 복귀시키는 함수
    	- modifyCube() : 각 명령어에 따라 큐브를 이동시키는 함수를
    	부르는 함수
    	- toLeft(), toRight(), toUp(), toBottom() : 큐브에서 움직일 행
    	또는 열을 파라미터로 받아와 해당 행 또는 열을 실질적으로 이동시키는 함수
    	- print() : 2차원배열의 큐브를 문자열로 엮는 함수

--- test2.js
+++ test2.js
@@ -1,1 +1,137 @@
-console.warn("step-2 commit test");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const init = `R R W\nG C W\nG B B\n`;
+const promptMsg = "\n명령어를 입력해주세요> ";
+let arr = init.split("\n").map((e) => e.split(" "));
+const order = {
+  toLeftOfUp: "U",
+  toRightOfUp: "u",
+  toUpOfRight: "R",
+  toDownOfRight: "r",
+  toDownOfLeft: "L",
+  toUpOfLeft: "l",
+  toRightOfBottom: "B",
+  toLeftOfBottom: "b",
+  quit: "Q",
+};
+rl.setPrompt(init + promptMsg);
+rl.prompt();
+rl.on("line", (line) => {
+  main(parse(line));
+});
+rl.on("close", () => {
+  process.exit();
+});
+
+function main(line) {
+  if (line === "Q") doQuit();
+  for (let i = 0; i < line.length; i++) {
+    const temp = modifyCube(line[i], arr);
+    const letter = recover(line[i]);
+    if (temp === -1) doQuit();
+    if (temp === undefined) {
+      console.log(`"${letter}" 명령어를 찾을 수 없습니다.`);
+      continue;
+    }
+    arr = temp;
+    console.log("\n" + letter + "\n" + print(arr));
+  }
+  rl.setPrompt(promptMsg);
+  rl.prompt();
+}
+
+function parse(line) {
+  return line.replace(/U'/g, "u").replace(/R'/g, "r").replace(/L'/g, "l").replace(/B'/g, "b");
+}
+
+function recover(letter) {
+  const codeNumber = letter.charCodeAt(0);
+  if (codeNumber >= 97 && codeNumber <= 122) return letter.toUpperCase() + "'";
+  return letter;
+}
+
+function doQuit() {
+  console.log("\nBye~");
+  rl.close();
+}
+
+function modifyCube(letter, arr) {
+  switch (letter) {
+    case order.toLeftOfUp:
+      arr = toLeft(arr, order.toLeftOfUp);
+      break;
+    case order.toLeftOfBottom:
+      arr = toLeft(arr, order.toLeftOfBottom);
+      break;
+    case order.toRightOfUp:
+      arr = toRight(arr, order.toRightOfUp);
+      break;
+    case order.toRightOfBottom:
+      arr = toRight(arr, order.toRightOfBottom);
+      break;
+    case order.toUpOfLeft:
+      arr = toUp(arr, order.toUpOfLeft);
+      break;
+    case order.toUpOfRight:
+      arr = toUp(arr, order.toUpOfRight);
+      break;
+    case order.toDownOfLeft:
+      arr = toDown(arr, order.toDownOfLeft);
+      break;
+    case order.toDownOfRight:
+      arr = toDown(arr, order.toDownOfRight);
+      break;
+    case order.quit:
+      return -1;
+    default:
+      return undefined;
+  }
+  return arr;
+}
+
+function toLeft(arr, flag) {
+  const row = flag === order.toLeftOfUp ? 0 : 2;
+  const temp = arr[row][1];
+  arr[row][1] = arr[row][2];
+  arr[row][2] = arr[row][0];
+  arr[row][0] = temp;
+  return arr;
+}
+
+function toRight(arr, flag) {
+  const row = flag === order.toRightOfUp ? 0 : 2;
+  const temp = arr[row][2];
+  arr[row][2] = arr[row][1];
+  arr[row][1] = arr[row][0];
+  arr[row][0] = temp;
+  return arr;
+}
+
+function toUp(arr, flag) {
+  const col = flag === order.toUpOfLeft ? 0 : 2;
+  const temp = arr[0][col];
+  arr[0][col] = arr[1][col];
+  arr[1][col] = arr[2][col];
+  arr[2][col] = temp;
+  return arr;
+}
+
+function toDown(arr, flag) {
+  const col = flag === order.toDownOfLeft ? 0 : 2;
+  const temp = arr[2][col];
+  arr[2][col] = arr[1][col];
+  arr[1][col] = arr[0][col];
+  arr[0][col] = temp;
+  return arr;
+}
+
+function print(arr) {
+  let res = "";
+  for (let i = 0; i < arr.length; i++) {
+    res += arr[i].join(" ") + "\n";
+  }
+  return res.trim();
+}
